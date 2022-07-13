0da8b6bf6be3db0c1496a75090ea3fee6789c1c4
Amartya Yadav <amartyadav@gmail.com>
2022-07-13 16:51:08 +0100

    Added IfEitherCommandNode.cs to support ParseIfEitherCommand()

---
+++ Compiler/Nodes/CommandNodes/IfEitherCommandNode.cs
@@ -0,0 +1,52 @@
+namespace Compiler.Nodes
+{
+    /// <summary>
+    /// A node corresponding to an ifeither command
+    /// </summary>
+    public class IfEitherCommandNode : ICommandNode
+    {
+        /// <summary>
+        /// The IfEither condition expression
+        /// </summary>
+        public IExpressionNode IfEitherExpression { get; }
+        
+        /// <summary>
+        /// The Or condition expression
+        /// </summary>
+        public IExpressionNode OrExpression { get; }
+        
+        /// <summary>
+        /// The then branch command
+        /// </summary>
+        public ICommandNode ThenCommand { get; }
+
+        /// <summary>
+        /// The else branch command
+        /// </summary>
+        public ICommandNode ElseCommand { get; }
+        
+        /// <summary>
+        /// The position in the code where the content associated with the node begins
+        /// </summary>
+        public Position Position { get; }
+
+        /// <summary>
+        /// Creates a new ifeither command node
+        /// </summary>
+        /// <param name="ifEitherExpression">The ifeither condition expression</param>
+        /// <param name="orExpression">The or condition expression</param>
+        /// <param name="thenCommand">The then branch command</param>
+        /// <param name="elseCommand">The else branch command</param>
+        /// <param name="position">The position in the code where the content associated with the node begins</param>
+
+        public IfEitherCommandNode(IExpressionNode ifEitherExpression, IExpressionNode orExpression,
+            ICommandNode thenCommand, ICommandNode elseCommand, Position position)
+        {
+            IfEitherExpression = ifEitherExpression;
+            OrExpression = orExpression;
+            ThenCommand = thenCommand;
+            ElseCommand = elseCommand;
+            Position = position;
+        }
+    }
+}
